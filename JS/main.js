1d77ccb178805dfeb26e9459f62d96effb6a8419
Jeffrey Mardoqueo <jeffreymardoqueo260@gmail.com>
2023-05-28 20:04:10 -0600

    Arregle la parte de los ejericios con comentarios

--- JS/main.js
+++ JS/main.js
@@ -1,18 +1,20 @@
+//----------------------Ejercicio para saber si una palabra empieza con Mayuscula o no xd
 let palabras = ["Jeffrey", "jeff"];
 function MayusculaoMinuscula(palabra) {
 return document.write(/^[a-z]/.test(palabra));
 }
-// MayusculaoMinuscula(palabras[1]);
+MayusculaoMinuscula(palabras[1]);
+
+//----------------------Ejercicio para saber si un año es bisiesto o no
 function esBisiesto(anio) {
 return (anio % 4 === 0 && anio % 100 !== 0) || anio % 400 === 0 ? true : false;
 }
-function esBisiesto1(anio){
-    return(anio % 4 === 0 && anio % 100 !== 0 || anio % 400 === 0? true : false );
-}
-
-function TablasdeMultiplicar(){
-    for (i = 1; i <= numeromultiplicar; i++) {
+document.write(esBisiesto(2000));
+document.write(esBisiesto(2001));
+//----------------------Ejercicio para realizar las tablas de multiplicar
+function TablasdeMultiplicar(numeromultiplicar){
+    for (i = 1; i <= 10; i++) {
         document.write(`${numeromultiplicar} * ${i} = ${numeromultiplicar * i} <br>`);
     }
 }
-TablasdeMultiplicar();
+TablasdeMultiplicar(2);
